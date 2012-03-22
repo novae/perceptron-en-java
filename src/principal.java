2647874150497d2e683cfd2e3921a2c0969b5e20
novaegr <novaegr@gmail.com>
2012-03-21 22:34:26 -0600

    Convenciones

--- src/principal.java
+++ src/principal.java
@@ -4,8 +4,8 @@ import javax.swing.JTextField;
 
 
 public class principal extends javax.swing.JFrame {
-int X1=0,X2=0;
-double W1,W2,BIAS,U,sumatoria;
+int entradaUno=0,entradaDos=0;
+double pesoSinapticoUno,pesoSinapticoDos,bias,u,sumatoria;
 
     
     public principal() {
@@ -18,17 +18,17 @@ double W1,W2,BIAS,U,sumatoria;
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        x1 = new javax.swing.JTextField();
+        X1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        x2 = new javax.swing.JTextField();
+        X2 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        w1 = new javax.swing.JTextField();
-        w2 = new javax.swing.JTextField();
+        W1 = new javax.swing.JTextField();
+        W2 = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        bias = new javax.swing.JTextField();
+        BIAS = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        u = new javax.swing.JTextField();
+        U = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
         YD = new javax.swing.JTextField();
         Entrenar = new javax.swing.JButton();
@@ -39,27 +39,27 @@ double W1,W2,BIAS,U,sumatoria;
 
         jLabel1.setText("x1");
 
-        x1.setText("0");
+        X1.setText("0");
 
         jLabel2.setText("x2");
 
-        x2.setText("1");
+        X2.setText("1");
 
         jLabel3.setText("w1");
 
         jLabel4.setText("w2");
 
-        w1.setText("3");
+        W1.setText("3");
 
-        w2.setText("3");
+        W2.setText("3");
 
         jLabel5.setText("bias");
 
-        bias.setText("45");
+        BIAS.setText("45");
 
         jLabel6.setText("u");
 
-        u.setText("2");
+        U.setText("2");
 
         jLabel7.setText("YD");
 
@@ -86,15 +86,15 @@ double W1,W2,BIAS,U,sumatoria;
                         .addGap(12, 12, 12)
                         .addComponent(jLabel2))
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(x1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(x2)))
+                        .addComponent(X1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(X2)))
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(w1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(w2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(W1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(W2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(58, 58, 58)
@@ -106,7 +106,7 @@ double W1,W2,BIAS,U,sumatoria;
                                 .addComponent(jLabel6))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(94, 94, 94)
-                                .addComponent(u, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(U, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
@@ -123,7 +123,7 @@ double W1,W2,BIAS,U,sumatoria;
                             .addComponent(YD, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(47, 47, 47)
-                        .addComponent(bias, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(BIAS, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
                         .addComponent(Entrenar)))
                 .addGap(23, 23, 23))
@@ -136,16 +136,16 @@ double W1,W2,BIAS,U,sumatoria;
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(x1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(X1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(66, 66, 66)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(x2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(X2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(35, 35, 35)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(w1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(u, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(W1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(U, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
@@ -153,7 +153,7 @@ double W1,W2,BIAS,U,sumatoria;
                         .addGap(18, 18, 18)
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(w2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(W2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(47, 47, 47)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -167,7 +167,7 @@ double W1,W2,BIAS,U,sumatoria;
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(bias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(BIAS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(Entrenar))
                         .addGap(5, 5, 5)))
                 .addContainerGap(25, Short.MAX_VALUE))
@@ -178,16 +178,17 @@ double W1,W2,BIAS,U,sumatoria;
 
     private void EntrenarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EntrenarActionPerformed
         try{
-        X1=Integer.parseInt(x1.getText());
-        X2=Integer.parseInt(x2.getText());
+        entradaUno=Integer.parseInt(X1.getText());
+        entradaDos=Integer.parseInt(X2.getText());
         
-        W1=Double.parseDouble(w1.getText());
-        W2=Double.parseDouble(w2.getText());
-        BIAS=Double.parseDouble(bias.getText());
-        U=Double.parseDouble(u.getText());
-        if((X1==0 || X1==1) && (X2==0 || X2==1)){
+        pesoSinapticoUno=Double.parseDouble(W1.getText());
+        pesoSinapticoDos=Double.parseDouble(W2.getText());
+        bias=Double.parseDouble(BIAS.getText());
+        u=Double.parseDouble(U.getText());
+        
+        if((entradaUno==0 ||entradaUno==1) && (entradaDos==0 || entradaDos==1)){
             JOptionPane.showMessageDialog(null, "aceptado");
-            System.out.println("val"+setU(getU()) );
+            
             
         }
         else{
@@ -210,69 +211,7 @@ double W1,W2,BIAS,U,sumatoria;
     return sumatoria;
     } 
     
-    public double getBIAS() {
-        return BIAS;
-    }
-    public double getU() {
-        return U;
-    }
-    public double setU(double U) {
-        this.U = getX1()*getW1()+getX2()*getW1()+getBIAS();
-        return U;
-    }
-    public double getW1() {
-        return W1;
-    }
-    public double getW2() {
-        return W2;
-    }
-    public int getX1() {
-        return X1;
-    }
-    public int getX2() {
-        return X2;
-    }
-    public JTextField getBiasTextField() {
-        return bias;
-    }
-    public JTextField getUTextField() {
-        return u;
-    }
-    public JTextField getW1TextField() {
-        return w1;
-    }
-    public JTextField getW2TextField() {
-        return w2;
-    }
-    public void setBias(JTextField bias) {
-        this.bias = bias;
-    }
-    public void setU(JTextField u) {
-        this.u = u;
-    }
-    public void setW1(JTextField w1) {
-        this.w1 = w1;
-    }
-    public void setW2(JTextField w2) {
-        this.w2 = w2;
-    }
-
-    public void setYR(JTextField YR) {
-        this.YR = YR;
-    }
-    public JTextField getYR() {
-        return YR;
-    }
-
-    public JTextField getYD() {
-        return YD;
-    }
-     public void setYD(JTextField YD) {
-        this.YD = YD;
-    }
     
-    
-
  
   
     
@@ -280,6 +219,13 @@ double W1,W2,BIAS,U,sumatoria;
     
     
     
+     
+     
+     
+     
+     
+     
+     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -313,10 +259,15 @@ double W1,W2,BIAS,U,sumatoria;
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField BIAS;
     private javax.swing.JButton Entrenar;
+    private javax.swing.JTextField U;
+    private javax.swing.JTextField W1;
+    private javax.swing.JTextField W2;
+    private javax.swing.JTextField X1;
+    private javax.swing.JTextField X2;
     private javax.swing.JTextField YD;
     private javax.swing.JTextField YR;
-    private javax.swing.JTextField bias;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -325,10 +276,5 @@ double W1,W2,BIAS,U,sumatoria;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JTextField u;
-    private javax.swing.JTextField w1;
-    private javax.swing.JTextField w2;
-    private javax.swing.JTextField x1;
-    private javax.swing.JTextField x2;
     // End of variables declaration//GEN-END:variables
 }
